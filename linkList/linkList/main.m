928390bcb3152042dc5a1c980ab00b0394a85f50
MChainZhou <18395891212@163.com>
2017-07-31 15:13:35 +0800

    初始化项目

---
+++ linkList/linkList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  linkList
+//
+//  Created by apple on 2017/7/31.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
